e6867e904837196897fe1a1ef23af0d286224c02
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-09 22:39:04 +0200

    re-typing methods and constructors into part 2 requirements

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
@@ -7,6 +7,16 @@ namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
         public string RecipeName { get; set; }
         public List<Ingredient> Ingredients { get; set; }
         public List<string> Steps { get; set; }
+        public Recipe(string recipeName)
+        {
+            RecipeName = recipeName;
+            Ingredients = new List<Ingredient>();
+            Steps = new List<string>();
+        }
 
+        public void AddIngredients(string name, int quantity,string unitOfMeasurement)
+        {
+
+        }
     }
 }
