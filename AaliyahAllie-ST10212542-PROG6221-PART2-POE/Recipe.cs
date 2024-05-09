621ea78ab5864c18ae22d093f84290df6612231a
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-09 22:47:39 +0200

    methods for add ingredients and steps created

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
@@ -16,7 +16,18 @@ namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
 
         public void AddIngredients(string name, int quantity,string unitOfMeasurement)
         {
+            Ingredients.Add(new Ingredient
+            {
+                Name = name,
+                Quantity = quantity,
+                OriginalQuantity = quantity,
+                UnitOfMeasurement = unitOfMeasurement
+            });
+        }
 
+        public void AddStep(string step)
+        {
+            Steps.Add(step);
         }
     }
 }
