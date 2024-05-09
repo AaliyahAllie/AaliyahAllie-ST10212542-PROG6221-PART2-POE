b0aa077b83c4d40a6771dd5cb55b0d4d415a8baa
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-09 22:18:02 +0200

    implementing usings for generic collection storing and changing namespace to accommodate changes from part 1 code to part 2 code

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
@@ -1,4 +1,6 @@
-﻿namespace AaliyahAllie_ST10212542_PROG6221_PART1
+﻿using System;
+using System.Collections.Generic;
+namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
 {
     
     public class Recipe
