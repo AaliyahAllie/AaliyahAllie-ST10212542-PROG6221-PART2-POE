0a08c90f68982b0f1a1a983fd9a5dc03688a9b75
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-10 17:34:30 +0200

    implementing getter and setter for RecipeCalorieNotification

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Recipe.cs
@@ -8,6 +8,7 @@ namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
         public string RecipeName { get; set; }
         public List<Ingredient> Ingredients { get; set; }
         public List<string> Steps { get; set; }
+        public RecipeCalorieNotification CalorieNotifer { get; set; };
         public Recipe(string recipeName)
         {
             RecipeName = recipeName;
