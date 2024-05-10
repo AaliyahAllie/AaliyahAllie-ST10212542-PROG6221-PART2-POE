9c3e071b20b453463fb4c3daf8e5ffc27324a09c
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-10 18:09:35 +0200

    Ingredients.cs created

---
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Ingredients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
+{
+    public class Ingredients
+    {
+    }
+}
