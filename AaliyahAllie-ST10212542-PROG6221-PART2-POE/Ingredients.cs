acce361a9230d974c8f7ffd3dd88ff1497ed0f49
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-10 18:15:01 +0200

    Populate ingredient.cs with variables to fix errors on recipe.cs

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Ingredients.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Ingredients.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
+﻿public class Ingredient
 {
-    public class Ingredients
-    {
-    }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public int OriginalQuantity { get; set; }
+    public string UnitOfMeasurement { get; set; }
+    public double Calories { get; set; }
+    public double OriginalCalories { get; set; } 
+    public string FoodGroup { get; set; }
+    public int FoodGroupNumber { get; set; } 
 }
