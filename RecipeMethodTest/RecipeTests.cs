a9799998d496a99b45528daf38b2611e7626f12b
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-11 22:31:59 +0200

    Populate with test logic

--- RecipeMethodTest/RecipeTests.cs
+++ RecipeMethodTest/RecipeTests.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace RecipeMethodTest
 {
     public class RecipeTests
@@ -8,7 +10,19 @@ namespace RecipeMethodTest
         [SetUp]
         public void Setup()
         {
-            
+            consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            recipe.newRecipe();
+            recipe.RecipeName = "Sample Recipe";
+            recipe.Ingredients = new List<Ingredient>
+            {
+                new Ingredient{OriginalQuantity = 100, Quantity = 100, OriginalCalories = 50, Calories = 50 },
+
+
+                new Ingredient {OriginalQuantity = 200, Quantity = 200, OriginalCalories = 80, Calories = 80}
+                };
+                
+            }
         }
 
         [Test]
