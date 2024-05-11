01c259bdbc2c6a7f9e79846370fb5d81aa2f6d5b
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-11 22:43:53 +0200

    TEST FOR CalorieNotification_PrintsGreen_WhenCaloriesInRange CREATED

--- RecipeMethodTest/RecipeTests.cs
+++ RecipeMethodTest/RecipeTests.cs
@@ -21,33 +21,46 @@ namespace RecipeMethodTest
 
                 new Ingredient {OriginalQuantity = 200, Quantity = 200, OriginalCalories = 80, Calories = 80}
                 };
-                
-            }
+
         }
+
 
         [TearDown]
         public void CalculateTotalCalories_ReturnsCorrectTotal()
         {
-        double expectedTotalCalories = 130;
+            double expectedTotalCalories = 130;
 
-        double actualTotalCalories = recipe.CalculateTotalCalories();
+            double actualTotalCalories = recipe.CalculateTotalCalories();
 
-        Assert.AreEqual(expectedTotalCalories, actualTotalCalories);
+            Assert.AreEqual(expectedTotalCalories, actualTotalCalories);
         }
 
         [Test]
         public void CalculateTotalCalories_ReturnsZero_WhenNoIngredients()
         {
-        recipe.Ingredients = new List<Ingredient>();
+            recipe.Ingredients = new List<Ingredient>();
 
-        double expectedTotalCalories = 0;
+            double expectedTotalCalories = 0;
 
-        double actualTotalCalories = recipe.CalculateTotalCalories();
+            double actualTotalCalories = recipe.CalculateTotalCalories();
 
-        Assert.AreEqual(expectedTotalCalories,actualTotalCalories);
+            Assert.AreEqual(expectedTotalCalories, actualTotalCalories);
         }
+
+        [Test]
+        public void CalorieNotification_PrintsGreen_WhenCaloriesInRange()
         {
-            
+            double totalCalories = 150;
+
+            string expectedOutput = $"Total Calories: {totalCalories} (Total calories of {recipe.RecipeName} are between 0 and 300. This is still in a healthy calorie range.)\r\n";
+
+            recipe.NotifyCalorieStatus(totalCalories);
+
+            Assert.AreEqual(ConsoleColor.Green, Console.ForegroundColor);
+            Assert.AreEqual(expectedOutput, consoleOutput.ToString());
+
         }
+
+
     }
 }
