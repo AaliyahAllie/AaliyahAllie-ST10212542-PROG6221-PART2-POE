a7c60ecc3acbf5408cc5a6ffe42f2f9e06a05402
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-11 22:15:19 +0200

    Creating Unit Tests

---
+++ RecipeMethodTest/RecipeTests.cs
@@ -0,0 +1,16 @@
+namespace RecipeMethodTest
+{
+    public class RecipeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
