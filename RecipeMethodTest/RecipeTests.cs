544583dd589cbcf498e67c9e61532a1fb781a418
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-11 23:11:31 +0200

    New unit test has errors. Will deal with error handling at later stage

--- RecipeMethodTest/RecipeTests.cs
+++ RecipeMethodTest/RecipeTests.cs
@@ -1,16 +1,51 @@
-namespace RecipeMethodTest
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace RecipeMethod.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class RecipeTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void CalculateTotalCalories_EmptyIngredients_ReturnsZero()
         {
+            // Arrange
+            Recipe recipe = new Recipe("TestRecipe");
+
+            // Act
+            double totalCalories = recipe.CalculateTotalCalories();
+
+            // Assert
+            Assert.AreEqual(0, totalCalories);
         }
 
         [Test]
-        public void Test1()
+        public void CalculateTotalCalories_SingleIngredient_ReturnsCorrectValue()
         {
-            Assert.Pass();
+            // Arrange
+            Recipe recipe = new Recipe("TestRecipe");
+            recipe.AddIngredients("Ingredient1", 100, "grams", 200);
+
+            // Act
+            double totalCalories = recipe.CalculateTotalCalories();
+
+            // Assert
+            Assert.AreEqual(200, totalCalories);
+        }
+
+        [Test]
+        public void CalculateTotalCalories_MultipleIngredients_ReturnsSumOfCalories()
+        {
+            // Arrange
+            Recipe recipe = new Recipe("TestRecipe");
+            recipe.AddIngredients("Ingredient1", 100, "grams", 200);
+            recipe.AddIngredients("Ingredient2", 50, "grams", 150);
+
+            // Act
+            double totalCalories = recipe.CalculateTotalCalories();
+
+            // Assert
+            Assert.AreEqual(350, totalCalories);
         }
     }
 }
