1d120a86961099468ef3b52411017051e8ba6af7
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-12 13:37:24 +0200

    Namespace

--- AaliyahAllie-ST10212542-PROG6221-PART2-POE/Ingredients.cs
+++ AaliyahAllie-ST10212542-PROG6221-PART2-POE/Ingredients.cs
@@ -1,11 +1,14 @@
-﻿public class Ingredient
+﻿namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
 {
-    public string Name { get; set; }
-    public int Quantity { get; set; }
-    public int OriginalQuantity { get; set; }
-    public string UnitOfMeasurement { get; set; }
-    public double Calories { get; set; }
-    public double OriginalCalories { get; set; } 
-    public string FoodGroup { get; set; }
-    public int FoodGroupNumber { get; set; } 
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int OriginalQuantity { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public double Calories { get; set; }
+        public double OriginalCalories { get; set; }
+        public string FoodGroup { get; set; }
+        public int FoodGroupNumber { get; set; }
+    }
 }
---
+++ RecipeTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RecipeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
