973e3fb5e8f0f7b85b6d8b9d221ca21ad201ad09
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-12 13:35:47 +0200

    Going to redo unit tests again

--- RecipeMethodTest/RecipeTests.cs
+++ RecipeMethodTest/RecipeTests.cs
@@ -1,51 +1,18 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 
-namespace RecipeMethod.Tests
+namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
 {
     [TestFixture]
     public class RecipeTests
     {
         [Test]
-        public void CalculateTotalCalories_EmptyIngredients_ReturnsZero()
-        {
-            // Arrange
-            Recipe recipe = new Recipe("TestRecipe");
-
-            // Act
-            double totalCalories = recipe.CalculateTotalCalories();
-
-            // Assert
-            Assert.AreEqual(0, totalCalories);
-        }
+        
 
         [Test]
-        public void CalculateTotalCalories_SingleIngredient_ReturnsCorrectValue()
-        {
-            // Arrange
-            Recipe recipe = new Recipe("TestRecipe");
-            recipe.AddIngredients("Ingredient1", 100, "grams", 200);
 
-            // Act
-            double totalCalories = recipe.CalculateTotalCalories();
-
-            // Assert
-            Assert.AreEqual(200, totalCalories);
-        }
 
         [Test]
-        public void CalculateTotalCalories_MultipleIngredients_ReturnsSumOfCalories()
-        {
-            // Arrange
-            Recipe recipe = new Recipe("TestRecipe");
-            recipe.AddIngredients("Ingredient1", 100, "grams", 200);
-            recipe.AddIngredients("Ingredient2", 50, "grams", 150);
-
-            // Act
-            double totalCalories = recipe.CalculateTotalCalories();
-
-            // Assert
-            Assert.AreEqual(350, totalCalories);
-        }
+      
     }
 }
