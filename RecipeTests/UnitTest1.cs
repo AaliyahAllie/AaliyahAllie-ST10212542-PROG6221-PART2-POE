cbb0e9fd391845d8c88e83960392f7fdbd593e42
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-12 13:57:19 +0200

    unit test to test calorie total if its empty

--- RecipeTests/UnitTest1.cs
+++ RecipeTests/UnitTest1.cs
@@ -6,6 +6,16 @@ namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
     [TestClass]
     public class UnitTest1
     {
+
+        [TestMethod]
+        public void TestCalculateTotalCalories_NoIngredients()
+        {
+            Recipe recipe = new Recipe("Test Recipe");
+
+            double totalCalories = recipe.CalculateTotalCalories();
+
+            Assert.AreEqual(0,totalCalories);
+        }
         [TestMethod]
         public void TestScaleRecipe()
         {
