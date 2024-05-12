4d84ecce5124e6ab62b0a3354c9c02e463c91d86
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-12 13:47:24 +0200

    unit test for scale of recipe

--- RecipeTests/UnitTest1.cs
+++ RecipeTests/UnitTest1.cs
@@ -1,11 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
 namespace AaliyahAllie_ST10212542_PROG6221_PART2_POE
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestScaleRecipe()
         {
+            // Arrange
+            Recipe recipe = new Recipe("Test Recipe");
+            recipe.AddIngredients("Ingredient 1", 100, "g", 50);
+            recipe.AddIngredients("Ingredient 2", 200, "ml", 100);
+
+            double factor = 2;
+
+            // Act
+            recipe.ScaleRecipe(factor);
+
+            // Assert
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                Assert.AreEqual(ingredient.OriginalQuantity * factor, ingredient.Quantity);
+                Assert.AreEqual(ingredient.OriginalCalories * factor, ingredient.Calories);
+            }
         }
     }
 }
